a155dfb0c03cf0d13d72af1302d6c135d92e3ae8
morrain <626100676@qq.com>
2018-01-29 19:20:13 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export * from 'proto-ui.vusion';
+
+import Select from './u-select.vue';
+import SelectItem from './u-select-item.vue';
+import Well from './u-well.vue';
+
+export {
+    Select,
+    SelectItem,
+    Well,
+};
